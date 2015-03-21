d945dc00027d3481d4372cd2ee3bebc6317c058a
nightbane30 <andrewpeterson99@gmail.com>
2015-03-21 16:32:12 -0400

    Huge Addition #1
    Added FPS controller, changed lighting and added water around platform.
    Added table and ability to generate up to 15 cups by pressing E.  You
    can use click to create a raycast to apply forces to gameobjects.  Also
    added ability to press R and restart the scene.  Lighting is messed up
    in the editor after restart but builds are fine.

---
+++ Assets/Raycast.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Raycast : MonoBehaviour {
+	
+	public float distance;
+	public float power;
+	public GameObject plat;
+	Vector3 platPosition;
+	public GameObject cup;
+	private Vector3 cupPosition;
+	public int amt;
+	private bool temp = true;
+	
+	// Use this for initialization
+	void Start () {
+		platPosition = new Vector3 (0, 30, 0);
+		cupPosition = new Vector3 (0, 15, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//If left mouse button is clicked
+		//create a raycast that originates from the mouse clicked position
+		//Button vals = 0 left click, 1 right click, 2 for mid click
+		if(Input.GetMouseButton(0)){
+			//This allows us to draw the ray
+			Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+			//if ray hits something, data gets stored in hitInfo variable
+			RaycastHit hitInfo;  
+			if(Physics.Raycast(rayOrigin, out hitInfo, distance)){
+				Debug.Log("You are casting a ray.");
+				Debug.DrawLine(rayOrigin.direction, hitInfo.point);
+				//if hitinfo.rigidbody is equal to something other than null
+				if(hitInfo.rigidbody != null){
+					//then add force at impact position of ray
+					hitInfo.rigidbody.AddForceAtPosition(rayOrigin.direction * power, hitInfo.point);
+					//This changes color
+					//hitInfo.transform.GetComponent<Renderer>().material.color = Color.green;
+					Instantiate(plat, platPosition, Quaternion.identity);
+					
+				}
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.R)){
+			Application.LoadLevel(Application.loadedLevelName);
+		}
+		if (Input.GetKeyDown (KeyCode.E)) {
+			amt += 1;
+			Instantiate(cup, cupPosition, Quaternion.identity);
+		}
+		if (amt >= 15) {
+			GameObject.Destroy(cup);
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+		
+		
+	}
+}
