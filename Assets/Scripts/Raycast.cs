aae5c0d690fd166b04e8e43fcfe027ffcd1b9098
nightbane30 <andrewpeterson99@gmail.com>
2015-03-22 11:10:30 -0400

    Balcony Addition, Raycast upgrade
    Balcony with textures added.  You can now pull objects towards you by
    using right click on mouse.  Music changed with wind SFX.  Ground
    texture changed.  Table surface color changed.  Balcony has colliders on
    floor, bars.  No colliders on fancy design parts yet or the grass pots.

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class Raycast : MonoBehaviour {
 	
 	public float distance;
-	public float power;
+	public float powerPush;
 	Vector3 platPosition;
 	public GameObject cup;
 	private Vector3 cupPosition;
 	public int amt;
 	private bool temp = true;
+	public float powerPull;
 	
 	// Use this for initialization
 	void Start () {
@@ -33,7 +34,26 @@ public class Raycast : MonoBehaviour {
 				//if hitinfo.rigidbody is equal to something other than null
 				if(hitInfo.rigidbody != null){
 					//then add force at impact position of ray
-					hitInfo.rigidbody.AddForceAtPosition(rayOrigin.direction * power, hitInfo.point);
+					hitInfo.rigidbody.AddForceAtPosition(rayOrigin.direction * powerPush, hitInfo.point);
+					//This changes color
+					//hitInfo.transform.GetComponent<Renderer>().material.color = Color.green;
+					//Instantiate(plat, platPosition, Quaternion.identity);
+					
+				}
+			}
+		}
+		if(Input.GetMouseButton(1)){
+			//This allows us to draw the ray
+			Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+			//if ray hits something, data gets stored in hitInfo variable
+			RaycastHit hitInfo;  
+			if(Physics.Raycast(rayOrigin, out hitInfo, distance)){
+				Debug.Log("You are casting a ray.");
+				Debug.DrawLine(rayOrigin.direction, hitInfo.point);
+				//if hitinfo.rigidbody is equal to something other than null
+				if(hitInfo.rigidbody != null){
+					//then add force at impact position of ray
+					hitInfo.rigidbody.AddForceAtPosition(rayOrigin.direction * powerPull, hitInfo.point);
 					//This changes color
 					//hitInfo.transform.GetComponent<Renderer>().material.color = Color.green;
 					//Instantiate(plat, platPosition, Quaternion.identity);
